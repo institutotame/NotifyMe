a415c189472561aea72f0182d20600e7ae16e8e3
institutotame <ignacio@institutotame.com>
2018-02-01 11:52:07 +0100

    Comentario añadido al mainactivity

--- app/src/main/java/com/example/tame/notifyme/MainActivity.java
+++ app/src/main/java/com/example/tame/notifyme/MainActivity.java
@@ -4,18 +4,13 @@ import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
-import android.support.annotation.RequiresApi;
-import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mNotifyButton = (Button) findViewById(R.id.notify);
+        mNotifyButton = findViewById(R.id.notify);
         mNotifyButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -39,7 +34,8 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-xdfsgasdas
+
+    //Method to send Notification
     public void sendNotification() {
 
         if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
